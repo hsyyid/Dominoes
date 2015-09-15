9ac1d4b8e62493397bae2c2da8bdebca0905f111
unknown <344668@427-C314-10-127.mcpsmd.org>
2015-09-15 09:18:50 -0400

    stuff

--- src/io/github/hsyyid/dominoes/Dominoes.java
+++ src/io/github/hsyyid/dominoes/Dominoes.java
@@ -1,5 +1,15 @@
 package io.github.hsyyid.dominoes;
 
 public class Dominoes {
+	static Stack stack = new Stack();
+	static Domino D;
 
+	public static void main(String args[]) {
+		System.out.println("Welcome to Dominoes!");
+		while (stack.getTotalDominoes() != 21) {
+			D = stack.drawFromStack();
+			System.out.println("Your Domino is "D.toString());
+		}
+		
+	}
 }
