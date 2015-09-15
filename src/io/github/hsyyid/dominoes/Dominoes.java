71905518d68f82638c3ddbebf3774be46c634858
unknown <348470@427-C314-10-126.mcpsmd.org>
2015-09-15 09:25:51 -0400

    FORMATTING

--- src/io/github/hsyyid/dominoes/Dominoes.java
+++ src/io/github/hsyyid/dominoes/Dominoes.java
@@ -1,13 +1,16 @@
 package io.github.hsyyid.dominoes;
 
-public class Dominoes {
-	public static void main(String args[]) {
+public class Dominoes
+{
+	public static void main(String args[])
+	{
 		Stack.createStack();
 		System.out.println("Welcome to Dominoes!");
-		while (Stack.getTotalDominoes() != 21) {
-			Domino d = Stack.drawFromStack();
-			System.out.println("Your Domino is " + d.getFirstVal() + " , " + d.getSecondVal());
-		}
 		
+		while (Stack.getTotalDominoes() != 21)
+		{
+			Domino d = Stack.drawFromStack();
+			System.out.println("Your Domino is " + d.getFirstVal() + ", " + d.getSecondVal());
+		}		
 	}
 }
