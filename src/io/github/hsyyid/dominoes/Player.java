585459bab12bd2a73a435fb1edcf76bed3f7ec4a
unknown <348470@427-C314-10-126.mcpsmd.org>
2015-09-16 08:49:17 -0400

    Player Class added

---
+++ src/io/github/hsyyid/dominoes/Player.java
@@ -0,0 +1,34 @@
+package io.github.hsyyid.dominoes;
+
+import java.util.ArrayList;
+
+public class Player
+{
+	private int id;
+	private ArrayList<Domino> stack = new ArrayList<Domino>();
+	
+	public Player(int id)
+	{
+		this.id = id;
+	}
+	
+	public void addDomino(Domino domino)
+	{
+		this.stack.add(domino);
+	}
+	
+	public int getID()
+	{
+		return this.id;
+	}
+	
+	public ArrayList<Domino> getStack()
+	{
+		return this.stack;
+	}
+	
+	public int getStackSize()
+	{
+		return this.stack.size();
+	}
+}
