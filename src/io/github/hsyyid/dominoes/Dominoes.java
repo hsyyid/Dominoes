2d1b85ef996ea6c2d818d0d144d64bd07d6e14be
unknown <344668@427-C314-10-127.mcpsmd.org>
2015-09-16 08:51:59 -0400

    Stuff

--- src/io/github/hsyyid/dominoes/Dominoes.java
+++ src/io/github/hsyyid/dominoes/Dominoes.java
@@ -1,16 +1,17 @@
 package io.github.hsyyid.dominoes;
 
-public class Dominoes
-{
-	public static void main(String args[])
-	{
+public class Dominoes {
+	public static void main(String args[]) {
 		Stack.createStack();
 		System.out.println("Welcome to Dominoes!");
-		
-		while (Stack.getTotalDominoes() != 21)
-		{
+		while (Stack.getTotalDominoes() != 21) {
 			Domino d = Stack.drawFromStack();
-			System.out.println("Your Domino is " + d.getFirstVal() + ", " + d.getSecondVal());
-		}		
+			System.out.println("Your Domino is " + d.getFirstVal() + " , " + d.getSecondVal());
+			if(d.getFirstVal() == d.getSecondVal())
+			{
+				System.out.println("You have a double domino, with the value " + d.getFirstVal());
+			}
+		}
+		
 	}
 }
