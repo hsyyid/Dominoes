b973217dd0b441d0818fb0fcd997f52893d810f1
unknown <344668@427-C314-10-127.mcpsmd.org>
2015-09-16 09:03:14 -0400

    potato

--- src/io/github/hsyyid/dominoes/Dominoes.java
+++ src/io/github/hsyyid/dominoes/Dominoes.java
@@ -9,14 +9,26 @@ public class Dominoes
 		while (Stack.getTotalDominoes() != 21) {
 			Domino d = Stack.drawFromStack();
 			System.out.println("Your Domino is " + d.getFirstVal() + " , " + d.getSecondVal());
-			if(d.getFirstVal() == d.getSecondVal())
+
+			int highestDouble = 0;
+
+			if(d.getFirstVal() == d.getSecondVal() && d.getFirstVal() == 6)
 			{
-				System.out.println("You have a double domino, with the value " + d.getFirstVal());
+				System.out.println("You have a double domino, with the value " + d.getFirstVal() + ", you will go first!");
+			}
+			else if(d.getFirstVal() == d.getSecondVal())
+			{
+				if(highestDouble < d.getFirstVal())
+				{
+					highestDouble = d.getFirstVal();
+				}
+				
+				System.out.println("You have a double domino, with the value " + d.getFirstVal() + " If this is the highest double, you will go first!");
 			}
 		}
-		
+
 	}
-	
+
 	public static void dealDomino(int amtOfDomino)
 	{
 		for(int i = 0; i < amtOfDomino; i++)
