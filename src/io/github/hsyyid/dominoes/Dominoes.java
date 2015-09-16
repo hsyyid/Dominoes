18ae079671e239ac129a91e21745c5cd5386124a
unknown <348470@427-C314-10-126.mcpsmd.org>
2015-09-16 09:17:24 -0400

    ADDED TUUSDJFAJSDF

--- src/io/github/hsyyid/dominoes/Dominoes.java
+++ src/io/github/hsyyid/dominoes/Dominoes.java
@@ -1,40 +1,79 @@
 package io.github.hsyyid.dominoes;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Dominoes
 {
+	private static ArrayList<Player> players = new ArrayList<Player>();
+
 	public static void main(String[] args)
 	{
-		Stack.createStack();
 		System.out.println("Welcome to Dominoes!");
-		while (Stack.getTotalDominoes() != 21) {
-			Domino d = Stack.drawFromStack();
-			System.out.println("Your Domino is " + d.getFirstVal() + " , " + d.getSecondVal());
 
-			int highestDouble = 0;
+		Scanner in = new Scanner(System.in);
+		Stack.createStack();
 
-			if(d.getFirstVal() == d.getSecondVal() && d.getFirstVal() == 6)
-			{
-				System.out.println("You have a double domino, with the value " + d.getFirstVal() + ", you will go first!");
-			}
-			else if(d.getFirstVal() == d.getSecondVal())
-			{
-				if(highestDouble < d.getFirstVal())
-				{
-					highestDouble = d.getFirstVal();
-				}
-				
-				System.out.println("You have a double domino, with the value " + d.getFirstVal() + " If this is the highest double, you will go first!");
-			}
+		System.out.println("How many players are playing? ");
+		int amtOfPlayers = in.nextInt();
+
+		for(int i = 1; i <= amtOfPlayers; i++)
+		{
+			players.add(new Player(i));
 		}
 
+		Player startingPlayer = pickWhoGoesFirst();
+
+		in.close();
 	}
 
-	public static void dealDomino(int amtOfDomino)
+	public static void dealDomino(int amtOfDomino, Player player)
 	{
 		for(int i = 0; i < amtOfDomino; i++)
 		{
 			Domino d = Stack.drawFromStack();
-			System.out.println("Domino is " + d.getFirstVal() + ", " + d.getSecondVal());
+			player.addDominoToStack(d);
+			System.out.println("You have drawn " + d.getFirstVal() + ", " + d.getSecondVal());
 		}	
+	}
+
+	public static Player pickWhoGoesFirst()
+	{
+		Player startingPlayer = null;
+		int highestVal = 0;
+
+		for(Player p : players)
+		{
+			System.out.println("Player " + p.getID() + ": ");
+			dealDomino(7, p);
+
+			for(Domino d : p.getStack())
+			{
+				if(d.getFirstVal() == 6 && d.getSecondVal() == 6)
+				{
+					startingPlayer = p;
+					System.out.println("Player " + startingPlayer.getID() + " will go first, with the highest double value of 6.");
+					return startingPlayer;
+				}
+				else if(d.getFirstVal() == d.getSecondVal())
+				{
+					if(highestVal < d.getFirstVal())
+					{
+						highestVal = d.getFirstVal();
+						startingPlayer = p;
+					}
+				}
+			}
+		}
+
+		if(startingPlayer != null)
+		{
+			System.out.println("Player " + startingPlayer.getID() + " will go first, with the highest double value of " + highestVal + ".");
+			return startingPlayer;
+		}
+		else
+		{
+			return players.get(1);
+		}
 	}
 }
