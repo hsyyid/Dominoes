3e22d25a648c4410565a4dd7c7ab8abf18fde788
unknown <348470@427-C314-10-126.mcpsmd.org>
2015-09-17 09:26:21 -0400

    asldflkadsjgadsgjaoiju32jasdfgadfs

--- src/io/github/hsyyid/dominoes/Dominoes.java
+++ src/io/github/hsyyid/dominoes/Dominoes.java
@@ -1,7 +1,17 @@
 package io.github.hsyyid.dominoes;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * This simulates a game of Dominoes
@@ -11,30 +21,37 @@ import java.util.Scanner;
  * @version 0.1
  *
  */
-public class Dominoes
+public class Dominoes extends Canvas implements Runnable
 {
 	private static ArrayList<Player> players = new ArrayList<Player>();
+	public static final int WIDTH = 320;
+	public static final int HEIGHT = WIDTH / 12 * 9;
+	public static final int SCALE = 2;
+	public static STATE State = STATE.MENU;
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private static boolean running = false;
+	private static Thread thread;
+	private static JFrame game;
+	
+	public static enum STATE
+	{
+		MENU, GAME
+	}
 
-	/**
-	 * Runs the game
-	 * @param args Not used.
-	 */
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Dominoes!");
-
 		Scanner in = new Scanner(System.in);
 		Stack.createStack();
 
 		System.out.println("How many players are playing? ");
 		int amtOfPlayers = in.nextInt();
-		
+
 		if(amtOfPlayers > 4)
 		{
 			System.out.print("Error! You cannot play this game with more than 4 people.");
 			return;
 		}
-		
+
 		for(int i = 1; i <= amtOfPlayers; i++)
 		{
 			players.add(new Player(i));
@@ -42,22 +59,45 @@ public class Dominoes
 
 		Player startingPlayer = pickWhoGoesFirst();
 		players.add(0, startingPlayer);
+
+		game = new JFrame("Dominoes");
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.setResizable(false);
+		game.setLocationRelativeTo(null);
+		game.setPreferredSize(new Dimension());
+		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.pack();
+		game.setVisible(true);
+		in.close();	
 		
-		boolean stop = false;
-		
-		while(!stop)
-		{
-			playGame();
-		}
-		
-		in.close();
+		Dominoes domino = new Dominoes();
+		domino.start();
+	}
+	
+	private synchronized void start() {
+		if (running)
+			return;
+
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+
+	}
+
+	public BufferedImage loadImage(String path) throws IOException
+	{
+		BufferedImage image = ImageIO.read(new File("H:\\Workspace\\Dominoes\\res\\background.png"));
+		return image;
 	}
 
 	public static void playGame()
 	{
+		
 		;
 	}
-	
+
 	/**
 	 * Deals the specified amount of dominoes to the specified player
 	 * @param amtOfDomino Amount of dominoes to deal.
@@ -114,6 +154,43 @@ public class Dominoes
 		else
 		{
 			return players.get(1);
+		}
+	}
+
+	public void loadTextures()
+	{
+		try
+		{
+			image = loadImage("/background.png");
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}	
+
+	public void run() 
+	{
+		System.out.println("Welcome to Dominoes!");
+
+		loadTextures();
+		
+		JPanel pane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+            	System.out.println("Stuff happen?");
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, null);
+            }
+        };
+        
+        game.add(pane);
+		
+		boolean stop = false;
+
+		while(!stop)
+		{
+			playGame();
 		}
 	}
 }
